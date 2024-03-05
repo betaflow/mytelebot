8d522fba2cb9cb5baf606f02f0289c4229943ce5
betaflow <84759415+betaflow@users.noreply.github.com>
2024-03-05 12:58:22 +0700

    Update init.sql

--- init.sql
+++ init.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS users (
 	id integer PRIMARY KEY AUTOINCREMENT,
 	chat_id integer UNIQUE NOT NULL,
 	username text,
-	isBot integer,
 	firstName text,
 	lastName text,
-	level text
+	status text,
+	level integer
 );
